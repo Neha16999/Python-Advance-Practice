8913f957c96debb2f75e0a0542988478bc15b742
Neha Rathi <39767135+Neha16999@users.noreply.github.com>
2020-05-24 21:55:12 +0530

    Updated TextMe.py
    Resolved Security issues

--- 9_TextMe/TextMe.py
+++ 9_TextMe/TextMe.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC49caeee858347a94dca66907effdf1cc'
-auth_token = '0e76ac1028e48638fbeca30940f143b2'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Love your work! This msg is automated by Python',
                               from_='+12082959768',
-                              to='+919405150370'
+                              to='mob no'
                           )
 
 print(message.sid)
